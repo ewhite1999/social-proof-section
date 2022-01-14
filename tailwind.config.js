dd5e5f994df176eeb4fa65abc16237b76712cdb4
“ewhite1999” <ewhite1999@gmail.com>
2022-01-14 09:49:08 +0000

    finish html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    colors: {
+      "very-dark-magenta": "hsl(300, 43%, 22%)",
+      "soft-pink": "hsl(333, 80%, 67%)",
+      "dark-grayish-magenta": "hsl(303, 10%, 53%)",
+      "light-grayish-magenta": "hsl(300, 24%, 96%)",
+      "white-neutral": "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      sans: ["Spartan", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
